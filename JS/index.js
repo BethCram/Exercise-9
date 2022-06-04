7a36e6ef6e15de33dbc18d141ccaf9a02d862d23
BethCram <102839011+BethCram@users.noreply.github.com>
2022-06-03 21:46:40 -0700

    TroubleshootFooter

--- JS/index.js
+++ JS/index.js
@@ -42,6 +42,7 @@ h1Element.text("Interactive Pet Viewer")
 
 //Create a variable `footerElement` that refers to the `<footer>`, then
 //change the HTML content of that element
+let footerElement = $("footer")
 footerElement.html("<small>All images from <a href=‘https://unsplash.com/'>unsplash.com</a></small>")
 
 
