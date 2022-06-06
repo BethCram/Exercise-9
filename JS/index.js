54ae90e309941de0cc0663cabf94a711ecb776b9
BethCram <102839011+BethCram@users.noreply.github.com>
2022-06-05 20:01:18 -0700

    troubleshoot footer

--- JS/index.js
+++ JS/index.js
@@ -39,10 +39,9 @@ h1Element.text("Interactive Pet Viewer");
 
 
 
-
 //Create a variable `footerElement` that refers to the `<footer>`, then
 //change the HTML content of that element
-let footerElement = ("footer");
+let footerElement = ('footer');
 console.log(footer);
 footer.html('<small>All images from <a href="https://unsplash.com/">unsplash.com</a></small>');
 
